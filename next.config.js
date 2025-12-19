66d10e47a64bfa9f5cb378a1c484ccbb81dd7ae9
Reviver <108503367+Reviver123456@users.noreply.github.com>
2025-12-19 16:35:37 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/test4',
-  assetPrefix: '/test4/',
+  basePath: '/test6',
+  assetPrefix: '/test6/',
 }
 
 module.exports = nextConfig
